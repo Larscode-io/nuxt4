49e7968dce9fba0b263e5052a23689744bfe7c3d
Lars Devocht <lars.devocht@const-court.be>
2024-10-06 12:16:43 +0200

    mailman demo page

---
+++ server/api/fmtest/checkServer.js
@@ -0,0 +1,84 @@
+import { useRuntimeConfig, defineEventHandler, createError } from "#imports";
+
+// https://github.com/unjs/ofetch?tab=readme-ov-file : Allow self-signed certificates
+import { Agent } from "undici";
+
+const unsecureFetch = $fetch.create({
+  dispatcher: new Agent({
+    connect: { rejectUnauthorized: false },
+  }),
+});
+
+export default defineEventHandler(async () => {
+  const { auServername } = useRuntimeConfig();
+
+  if (!auServername) {
+    console.error("auServername is not defined in the runtime configuration.");
+    throw createError({
+      statusCode: 500,
+      message: "Server configuration error.",
+    });
+  }
+
+  const url = `https://${auServername}/fmi/data/vLatest/productInfo`;
+
+  // Helper function to handle fetch and return consistent format
+  async function fetchData(fetchFn) {
+    try {
+      await fetchFn(url, {
+        method: "GET",
+        timeout: 5000,
+      });
+
+      return {
+        status: "success",
+        code: 200,
+        message: "Data fetched successfully",
+        // data: response,
+      };
+    } catch (error) {
+      if (error.name === "FetchError") {
+        if (
+          error.cause?.message === "This operation was aborted" &&
+          error.cause?.code === 20
+        ) {
+          return {
+            status: "error",
+            code: 500,
+            message: "Our request timed out",
+            data: null,
+          };
+        } else if (
+          error.cause?.cause?.code === "CERT_HAS_EXPIRED" &&
+          error.cause?.cause?.message === "certificate has expired"
+        ) {
+          return {
+            status: "error",
+            code: 500,
+            message: "The certificate has expired",
+            data: null,
+          };
+        }
+      }
+      // Log the detailed error
+      console.error("Detailed error:", error);
+      return {
+        status: "error",
+        code: 500,
+        message: "Failed to connect to FileMaker database",
+        data: null,
+        error,
+      };
+    }
+  }
+
+  // First try using $fetch
+  let result = await fetchData($fetch);
+  // If $fetch fails due to a certificate issue, fallback to unsecureFetch
+  if (result.status === "error" && result.message.includes("certificate")) {
+    console.error("Fallback to unsecureFetch due to SSL error");
+    result = await fetchData(unsecureFetch);
+  }
+
+  return result;
+});
---
+++ server/services/fileMakerService.js
@@ -0,0 +1,113 @@
+// server/services/fileMakerService.js
+
+export class FileMakerService {
+  constructor(config, fetchFn) {
+    this.config = config;
+    this.fetch = fetchFn;
+    this.makeRequest = this.makeRequest.bind(this);
+    this.baseUrl = `https://${this.config.auServername}/fmi/data/v1/databases/${this.config.auDatabase}/layouts`;
+  }
+
+  async makeRequest(layout, token, query, sort, additionalParams = {}) {
+    const url = `${this.baseUrl}/${layout}/_find`;
+    const body = {
+      query,
+      sort,
+      "layout.response": layout,
+      ...additionalParams,
+    };
+
+    try {
+      return await this.fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      console.error(`Error in ${layout} request:`, error);
+      throw error;
+    }
+  }
+
+  async getPublicaties_Jaarverslagen(token) {
+    return this.makeRequest(
+      "Publicaties_Jaarverslagen",
+      token,
+      [{ "Jaarverslagen::offline": "==" }],
+      [{ fieldName: "Jaarverslagen::_k1_Jaarverslag_id", sortOrder: "descend" }]
+    );
+  }
+
+  async getPublicaties_Persberichten(token, lang, descriptionValue) {
+    return this.makeRequest(
+      "Publicaties_Persberichten",
+      token,
+      [
+        {
+          "Persberichten::offline": "==",
+          [lang]: descriptionValue,
+          "Persberichten::type": "AG",
+        },
+      ],
+      [{ fieldName: "Persberichten::_k1_Persbericht_id", sortOrder: "descend" }]
+    );
+  }
+
+  async getPublicaties_Studies(token, lang, descriptionValue) {
+    return this.makeRequest(
+      "Publicaties_Studies",
+      token,
+      [
+        {
+          "Studies::offline": "==",
+          [lang]: descriptionValue,
+        },
+      ],
+      [{ fieldName: "Studies::_k1_Studie_id", sortOrder: "descend" }]
+    );
+  }
+
+  async getArrestDateMinus10(token) {
+    // we don't use makeRequest because it the endpoint has a different structure
+    const url = `${this.baseUrl}/popup_XML_FOD_Justitie_Arrest/records?_offset=1&_limit=1`;
+
+    try {
+      const response = await this.fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async findJuportalData(token, arrestDate) {
+    arrestDate = arrestDate ?? "09/19/2024"; // default date is demo data
+    return this.makeRequest(
+      "popup_XML_FOD_Justitie_Arrest",
+      token,
+      [{ Arrest_Datum: arrestDate }],
+      [
+        { fieldName: "Arrest_Datum", sortOrder: "descend" },
+        { fieldName: "Arrest_Sorteernummer", sortOrder: "descend" },
+      ],
+      { limit: "200" }
+    );
+  }
+
+  async getPublicaties_Brochures(token) {
+    return this.makeRequest(
+      "Publicaties_Brochures",
+      token,
+      [{ "Brochures::offline": "==" }],
+      [{ fieldName: "Brochures::_k1_Brochure_id", sortOrder: "descend" }]
+    );
+  }
+}
