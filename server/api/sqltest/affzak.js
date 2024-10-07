419fa5a5f4ff5ea3c96f30e9d3e5bd6ead67a3ab
Lars Devocht <lars.devocht@const-court.be>
2024-10-07 13:30:41 +0200

    demo pages: mailman.vue and affzak.js

---
+++ server/api/sqltest/affzak.js
@@ -0,0 +1,30 @@
+import { useRuntimeConfig, defineEventHandler } from '#imports';
+import knex from 'knex';
+
+export default defineEventHandler(async () => {
+  const config = useRuntimeConfig();
+
+  const db = knex({
+    client: 'mysql2',
+    connection: {
+      host: config.dbHost,
+      user: config.dbUser,
+      password: config.dbPassword,
+      database: config.dbName,
+    },
+  });
+
+  const d = new Date();
+  d.setHours(0, 0, 0, 0);
+  const curDate = d.toISOString();
+
+  const records = await db('xaffzak')
+    .select([
+      'id_role as idRole',
+      db.raw("DATE_FORMAT(d_pron_fixe,'%Y-%m-%d') as dPronFixe")
+    ])
+    .where('d_pron_fixe', '>=', curDate)
+    .whereNotNull('d_pron_fixe');
+
+  return records;
+});
