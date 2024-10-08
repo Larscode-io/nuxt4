d47370b695b9cb95d5a3a25713fa86572f72d5fc
Lars Devocht <lars.devocht@const-court.be>
2024-10-08 18:21:05 +0200

    Nuxt Content demo pages

---
+++ app/pages/auth/callback.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const { code, state, scope, iss, client_id } = route.query
+</script>
+
+<template>
+  <div>
+    <h1>Callback</h1>
+    <p>code: {{ code }}</p>
+    <p>state: {{ state }}</p>
+    <p>scope: {{ scope }}</p>
+    <p>iss: {{ iss }}</p>
+    <p>client_id: {{ client_id }}</p>
+  </div>
+</template>
+
