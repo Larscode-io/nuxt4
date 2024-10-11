ed4ceb80f12bfef25c84e44eb6c5b65ebf1ea8c8
Lars Devocht <lars.devocht@const-court.be>
2024-10-11 13:27:43 +0200

    Basic starter templates

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+// app.vue is the top-level component that wraps everything.
+<template>
+  <NuxtLayout>
+    <header></header>
+    <main>
+      <NuxtPage />
+    </main>
+    <footer></footer>
+  </NuxtLayout>
+</template>
+
+<script setup>
+import { NuxtLayout, NuxtPage } from '#components'
+</script>
+
+<style scoped></style>
---
+++ app/pages/admin.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+  layout: 'admin',
+})
+</script>
+
+<template>
+  <div>
+    <h1>Admin Page</h1>
+  </div>
+</template>
