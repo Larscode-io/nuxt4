a9229edf68769a1370ecd75e9d19dc15bd75617b
Lars Devocht <lars.devocht@gmail.com>
2024-11-11 21:23:01 +0100

    test

--- app/components/FotoComponentPrize.vue
+++ app/components/FotoComponentPrize.vue
@@ -10,6 +10,14 @@
         :max-width="maxWidth"
       />
     </v-row>
+    <v-row>
+      <img
+        v-row
+        :src="i"
+        :alt="alt"
+        :max-width="maxWidth"
+      >
+    </v-row>
   </v-container>
 </template>
 
