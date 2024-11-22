01071073338a3dacbc05b913b4df41f54b7b6035
Lars Devocht <lars.devocht@gmail.com>
2024-11-22 13:51:59 +0100

    refactor: simplify brochure data handling and improve variable naming

--- app/pages/court/publications/brochures.vue
+++ app/pages/court/publications/brochures.vue
@@ -37,7 +37,6 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 // todo: error card ?
 import BannerImage from '~/components/BannerImage.vue'
 import img from '~/assets/img/newsletter-background-opt.png'
@@ -45,18 +44,16 @@ import { ApiUrl } from '~/core/constants'
 
 import { useLanguage } from '@/composables/useLanguage'
 
-definePageMeta({ layout: 'default' })
 const { t, locale } = useLanguage()
+
 const config = useRuntimeConfig()
 const baseURL = config.public.apiBaseUrl
 
 // during development, if the apiBaseUrl is not set in .env, the legacy server URL node04 will be used (nuxt.config.ts).
-const { data, error } = useLazyFetch(`${baseURL}${ApiUrl.publicationsBrochures}?lang=${locale.value}`)
+const { data: brochures, error } = useLazyFetch(`${baseURL}${ApiUrl.publicationsBrochures}?lang=${locale.value}`)
 if (error.value) {
   console.error(error.value)
 }
-const brochures = computed(() => data.value)
-
 useHead({
   title: t('menu.court.publications.brochure') || '',
   meta: [
