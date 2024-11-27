6bbc0ed3e715255006998c3c62c830fafe3cf7a6
Lars Devocht <lars.devocht@gmail.com>
2024-11-27 13:08:23 +0100

    feat: add click event to close menu on submenu title interaction

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -67,6 +67,7 @@
                               <v-col
                                 class="submenu-title bold"
                                 align="start"
+                                @click="closeMenu"
                               >
                                 {{ subItem.title }}
                               </v-col>
