f008330e2aef969b3fc1d93caff565bb23de77b9
Lars Devocht <lars.devocht@gmail.com>
2024-11-27 16:14:36 +0100

    feat: refactor ContentPage component template for improved structure and readability

--- app/components/ContentPage.vue
+++ app/components/ContentPage.vue
@@ -1,41 +1,3 @@
-<template>
-  <div>
-    <BannerImage
-      v-if="page"
-      :title="page?.title || ''"
-      :description="page?.description"
-      :image="img"
-      alt=""
-    />
-    <v-container class="flex-column align-start flex-nowrap">
-      <v-row>
-        <v-col
-          v-if="hasSidebarLinks"
-          cols="12"
-          md="4"
-        >
-          <Sidebar
-            :active="currentActiveContentInToc"
-            :toc="sideBarLinks"
-            @click="updateCurrentActiveContentInToc"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="8"
-        >
-          <article v-if="page">
-            <ContentRendererMarkdown
-              :value="page.body || {}"
-              class="nuxt-content content-renderer"
-            />
-          </article>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { ref, computed, onMounted, onUpdated, defineProps } from 'vue'
 import img from '~/assets/img/newsletter-background-opt.png'
@@ -55,7 +17,7 @@ const { data: page } = await useAsyncData('content', async () => {
       .findOne()
     const idsTo = doc.body?.toc?.links?.map(toc => toc.id) || []
     currentActiveContentInToc.value
-            = hash && idsTo.includes(hash) ? hash : idsTo[0] || ''
+      = hash && idsTo.includes(hash) ? hash : idsTo[0] || ''
     return doc
   }
   catch (error) {
@@ -121,6 +83,44 @@ onUpdated(() => {
   startIntersectionObserver()
 })
 </script>
+
+<template>
+  <div>
+    <BannerImage
+      v-if="page"
+      :title="page?.title || ''"
+      :description="page?.description"
+      :image="img"
+      alt=""
+    />
+    <v-container class="flex-column align-start flex-nowrap">
+      <v-row>
+        <v-col
+          v-if="hasSidebarLinks"
+          cols="12"
+          md="4"
+        >
+          <Sidebar
+            :active="currentActiveContentInToc"
+            :toc="sideBarLinks"
+            @click="updateCurrentActiveContentInToc"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          md="8"
+        >
+          <article v-if="page">
+            <ContentRendererMarkdown
+              :value="page.body || {}"
+              class="nuxt-content content-renderer"
+            />
+          </article>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .container {
