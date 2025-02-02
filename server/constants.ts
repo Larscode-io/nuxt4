3db2d5959c71566944ba9a5514bcd6365d63733f
nelisni-jstack <nicolas.nelissen@jstack.eu>
2025-02-02 16:26:01 +0100

    Judgment search WIP

--- server/constants.ts
+++ server/constants.ts
@@ -1,6 +1,8 @@
 // import { RoutePathKeys } from '../core/constants'
 // import { i18nKeys } from '../lang/keys'
 
+import { RoutePathKeys } from '~/core/constants'
+
 export enum Languages {
   ENGLISH = 'en',
   FRENCH = 'fr',
@@ -21,29 +23,29 @@ export const HttpErrorResponseCodeToTranslateLabelKey = {}
 
 export const EMPTY_VALUE = '----'
 
-// export const searchTabs = [
-//   {
-//     id: i18nKeys.general.message.judgment,
-//     to: RoutePathKeys.searchJudgment,
-//   },
-//   {
-//     id: i18nKeys.general.message.standard,
-//     to: RoutePathKeys.searchStandard,
-//   },
-//   {
-//     id: i18nKeys.general.message.systematicTableContentsLabel,
-//     to: RoutePathKeys.searchTableOfContent,
-//   },
-//   {
-//     id: i18nKeys.general.message.judgmentKeywordsSummary,
-//     to: RoutePathKeys.searchJudgmentKeywordSummary,
-//   },
-//   {
-//     id: i18nKeys.general.message.fullTextOfJudgments,
-//     to: RoutePathKeys.searchFullTextJudgment,
-//   },
-//   {
-//     id: i18nKeys.general.message.keywordsJudgmentsPendingCases,
-//     to: RoutePathKeys.searchJudgmentsAndPendingCases,
-//   },
-// ]
+/* export const searchTabs = [
+  {
+    id: 'general.message.judgment',
+    to: RoutePathKeys.searchJudgment,
+  },
+  {
+    id: 'general.message.standard',
+    to: RoutePathKeys.searchStandard,
+  },
+  {
+    id: 'general.message.systematic-table-contents-label',
+    to: RoutePathKeys.searchTableOfContent,
+  },
+  {
+    id: 'general.message.judgment-keywords-summary',
+    to: RoutePathKeys.searchJudgmentKeywordSummary,
+  },
+  {
+    id: 'general.message.full-text-of-judgments',
+    to: RoutePathKeys.searchFullTextJudgment,
+  },
+  {
+    id: 'general.message.keywords-judgments-pending-cases',
+    to: RoutePathKeys.searchJudgmentsAndPendingCases,
+  },
+] */
