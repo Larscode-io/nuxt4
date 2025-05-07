62836b0c8a31c21c3e2c69430f7021afcfd50978
Larscode-io <lars.devocht@const-court.be>
2025-05-07 15:52:42 +0200

    feat: simplify disclaimer and privacy policy pages by replacing complex structure with ContentPage component

--- app/pages/footer/privacy-policy.vue
+++ app/pages/footer/privacy-policy.vue
@@ -1,45 +1,13 @@
 <!-- Content based Page -->
 <template>
-  <div>
-    <BannerImage
-      v-if="page"
-      :title="page?.title || ''"
-      :description="page?.description"
-      :image="img"
-      alt=""
-    />
-    <v-container>
-      <v-row>
-        <v-col>
-          <article v-if="page">
-            <ContentRendererMarkdown
-              :value="page.body || {}"
-              class="nuxt-content content-renderer"
-            />
-          </article>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <ContentPage
+    :content-path="`${ContentKeys.privacyPolicy}`"
+    :img
+    :enable-toc="false"
+  />
 </template>
 
 <script setup lang="ts">
+import { ContentKeys } from '../../core/constants'
 import img from '~/assets/img/banner-media.png'
-
-import { useLanguage } from '@/composables/useLanguage'
-import { ContentKeys } from '~/core/constants'
-
-const { locale } = useLanguage()
-
-const { data: page } = await useAsyncData('content', async () => {
-  try {
-    const doc = await queryContent(`${locale.value}/${ContentKeys.privacyPolicy}`)
-      .findOne()
-    return doc
-  }
-  catch (error) {
-    console.error('Error fetching content:', error)
-    return null
-  }
-})
 </script>
