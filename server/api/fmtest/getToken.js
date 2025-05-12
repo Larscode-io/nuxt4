b59692ed03c9761e7848af949fdf35c8e0ec990e
Larscode-io <lars.devocht@const-court.be>
2025-05-12 22:49:37 +0200

    refactor: standardize error handling and improve code consistency across API files

--- server/api/fmtest/getToken.js
+++ server/api/fmtest/getToken.js
@@ -1,108 +1,112 @@
 // api/getToken.js
 
-import { useRuntimeConfig, defineEventHandler, createError } from "#imports";
-import { Agent } from "undici";
+import { Agent } from 'undici'
+import { useRuntimeConfig, defineEventHandler, createError } from '#imports'
+
 const unsecureFetch = $fetch.create({
   dispatcher: new Agent({
     connect: { rejectUnauthorized: false },
   }),
-});
+})
 export default defineEventHandler(async (event) => {
-  const { auServername, auDatabase, auUsername, auPassword } =
-    useRuntimeConfig();
+  const { auServername, auDatabase, auUsername, auPassword }
+    = useRuntimeConfig()
 
   if (!auServername || !auDatabase || !auUsername || !auPassword) {
     console.error(
-      "/api/getToken: FileMaker environment variables are not set."
-    );
+      '/api/getToken: FileMaker environment variables are not set.',
+    )
     throw createError({
       statusCode: 500,
-      message: "FileMaker environment variables are not set.",
-    });
+      message: 'FileMaker environment variables are not set.',
+    })
   }
-  const url = `https://${auServername}/fmi/data/v1/databases/${auDatabase}/sessions`;
+  const url = `https://${auServername}/fmi/data/v1/databases/${auDatabase}/sessions`
 
-//  async function fetchData(fetchFn) {
+  //  async function fetchData(fetchFn) {
   async function fetchData() {
     try {
       const response = await unsecureFetch(url, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
-          Authorization: `Basic ${btoa(`${auUsername}:${auPassword}`)}`,
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${btoa(`${auUsername}:${auPassword}`)}`,
         },
         timeout: 5000,
-      });
+      })
       // log if we are running server side or client side using nuxt 3 params
       if (import.meta.server) {
-        console.log("Running server side");
+        console.log('Running server side')
       }
       if (import.meta.client) {
-        console.log("Running client side");
+        console.log('Running client side')
       }
       return {
-        status: "success",
+        status: 'success',
         code: 200,
-        message: "Data fetched successfully",
+        message: 'Data fetched successfully',
         data: response,
-      };
-    } catch (error) {
-      if (error.name === "FetchError") {
+      }
+    }
+    catch (error) {
+      if (error.name === 'FetchError') {
         if (
-          error.cause?.message === "This operation was aborted" &&
-          error.cause?.code === 20
+          error.cause?.message === 'This operation was aborted'
+          && error.cause?.code === 20
         ) {
           return {
-            status: "error",
+            status: 'error',
             code: 408,
-            message: "Our request timed out",
+            message: 'Our request timed out',
             data: null,
-          };
-        } else if (
-          error.cause?.cause?.code === "CERT_HAS_EXPIRED" &&
-          error.cause?.cause?.message === "certificate has expired"
+          }
+        }
+        else if (
+          error.cause?.cause?.code === 'CERT_HAS_EXPIRED'
+          && error.cause?.cause?.message === 'certificate has expired'
         ) {
           return {
-            status: "error",
+            status: 'error',
             code: 400,
-            message: "The certificate has expired",
+            message: 'The certificate has expired',
             data: null,
-          };
+          }
         }
       }
       // Log the detailed error
-      console.error("Detailed error:", error);
+      console.error('Detailed error:', error)
       return {
-        status: "error",
+        status: 'error',
         code: 502,
-        message: "Failed to connect to FileMaker database",
+        message: 'Failed to connect to FileMaker database',
         data: null,
         error,
-      };
+      }
     }
   }
 
   try {
-    let result = await fetchData($fetch);
+    let result = await fetchData($fetch)
 
-    if (result.status === "error" && result.message.includes("certificate")) {
-      console.error("Fallback to unsecureFetch due to SSL error");
-      result = await fetchData(unsecureFetch);
+    if (result.status === 'error' && result.message.includes('certificate')) {
+      console.error('Fallback to unsecureFetch due to SSL error')
+      result = await fetchData(unsecureFetch)
     }
 
     // Set CORS headers
-    event.res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin (adjust for production)
-    event.res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allow specific HTTP methods
+    event.res.setHeader('Access-Control-Allow-Origin', '*') // Allow requests from any origin (adjust for production)
+    event.res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS') // Allow specific HTTP methods
     event.res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    )
 
-    return result;
-  } catch (error) {
+    return result
+  }
+  catch (error) {
     throw createError({
       statusCode: 500,
       message: `Failed to get token: ${error.message}`,
-    });
+    })
   }
-});
+})
