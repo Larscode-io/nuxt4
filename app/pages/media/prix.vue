8c43d5d01d96a4223a995b3dcd2676e7c4d03aac
Larscode-io <lars.devocht@const-court.be>
2025-05-12 16:19:40 +0200

    test

--- app/pages/media/prix.vue
+++ app/pages/media/prix.vue
@@ -96,7 +96,7 @@ const getImagePath = (imageName: string) => {
 // Carousel arrow and dot color override for Vuetify 3
 ::v-deep(.v-carousel) {
   .v-btn--icon {
-    color: rgb(47, 0, 255) !important;
+    color: rgb(0, 255, 145) !important;
   }
 
   .v-carousel__controls__prev,
