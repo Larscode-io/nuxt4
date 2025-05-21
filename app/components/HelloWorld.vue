0968a78e572a616cd3e95f4da0c5c3ebcc4c94b0
Larscode-io <lars.devocht@const-court.be>
2025-05-21 21:21:08 +0200

    feat: add HelloWorld component and corresponding unit tests for rendering

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<!-- components/HelloWorld.vue -->
+<template>
+  <div>
+    <h1>Hello, {{ name }}!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ name: string }>()
+</script>
