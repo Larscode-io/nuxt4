efcdad4e25d9b412d489c93cf1a793c06e898bef
Larscode-io <lars.devocht@const-court.be>
2025-05-25 19:01:20 +0200

    fallback head

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+// app.config.ts
+export default defineAppConfig({
+  app: {
+    head: {
+      title: 'Grondwettelijk Hof',
+      meta: [
+        { name: 'description', content: 'Officiële website van het Grondwettelijk Hof van België' },
+      ],
+    },
+  },
+})
