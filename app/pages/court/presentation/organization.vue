cd322a02d3d1f353ad851209066a746b872b0a97
Larscode-io <lars.devocht@const-court.be>
2025-06-01 16:02:07 +0200

    feat: center judge images in presentation files for better alignment

--- app/pages/court/presentation/organization.vue
+++ app/pages/court/presentation/organization.vue
@@ -181,43 +181,26 @@ onMounted(() => {
           md="8"
         >
           <v-row>
-            <article v-if="pageJudge">
-              <ContentRenderer
-                :value="pageJudge.body"
-                class="nuxt-content"
+            <template v-if="pending">
+              <v-skeleton-loader
+                type="heading, text, text, text, text, text"
+                class="mb-4"
+                :loading="pending"
+                height="400"
+                width="100%"
+                style="width: 100%; min-width: 100%;"
               />
-            </article>
-
-            <div
-              v-for="(member, index) in judgeMembers"
-              :key="member!.slug"
-              class="gallery"
-              :class="{ 'left-column': index % 2 === 0, 'right-column': index % 2 !== 0 }"
-            >
-              <MemberCard
-                :headline-level="2"
-                :name="member!.name"
-                :slug="member!.slug"
-                :image="member!.picture"
-                :with-image="true"
-                :infos="member!.infos"
-                :job-title="member!.mostRecentRole?.role"
-                :width="300"
-                :is-alive="true"
-                :lang="member!.lang"
-                :female-title="member!.femaleTitle"
-              />
-            </div>
-
-            <ClientOnly>
-              <article v-if="pageReferendar">
+            </template>
+            <template v-else>
+              <article v-if="pageJudge">
                 <ContentRenderer
-                  :value="pageReferendar.body"
+                  :value="pageJudge.body"
                   class="nuxt-content"
                 />
               </article>
+
               <div
-                v-for="(member, index) in officeStaffMembers"
+                v-for="(member, index) in judgeMembers"
                 :key="member!.slug"
                 class="gallery"
                 :class="{ 'left-column': index % 2 === 0, 'right-column': index % 2 !== 0 }"
@@ -231,191 +214,220 @@ onMounted(() => {
                   :infos="member!.infos"
                   :job-title="member!.mostRecentRole?.role"
                   :width="300"
-                  :is-alive="member!.isAlive"
+                  :is-alive="true"
                   :lang="member!.lang"
                   :female-title="member!.femaleTitle"
                 />
               </div>
 
-              <article v-if="pageClerk">
-                <ContentRenderer
-                  :value="pageClerk.body"
-                  class="nuxt-content"
-                />
-              </article>
+              <ClientOnly>
+                <article v-if="pageReferendar">
+                  <ContentRenderer
+                    :value="pageReferendar.body"
+                    class="nuxt-content"
+                  />
+                </article>
+                <div
+                  v-for="(member, index) in officeStaffMembers"
+                  :key="member!.slug"
+                  class="gallery"
+                  :class="{ 'left-column': index % 2 === 0, 'right-column': index % 2 !== 0 }"
+                >
+                  <MemberCard
+                    :headline-level="2"
+                    :name="member!.name"
+                    :slug="member!.slug"
+                    :image="member!.picture"
+                    :with-image="true"
+                    :infos="member!.infos"
+                    :job-title="member!.mostRecentRole?.role"
+                    :width="300"
+                    :is-alive="member!.isAlive"
+                    :lang="member!.lang"
+                    :female-title="member!.femaleTitle"
+                  />
+                </div>
 
-              <div
-                v-for="(member, index) in registrarMembers"
-                :key="member!.slug"
-                class="gallery"
-                :class="{ 'left-column': index % 2 === 0, 'right-column': index % 2 !== 0 }"
-              >
-                <MemberCard
-                  :headline-level="2"
-                  :name="member!.name"
-                  :slug="member!.slug"
-                  :image="member!.picture"
-                  :with-image="true"
-                  :infos="member!.infos"
-                  :job-title="member!.mostRecentRole?.role"
-                  :width="300"
-                  :is-alive="member!.isAlive"
-                  :lang="member!.lang"
-                  :female-title="member!.femaleTitle"
-                />
-              </div>
+                <article v-if="pageClerk">
+                  <ContentRenderer
+                    :value="pageClerk.body"
+                    class="nuxt-content"
+                  />
+                </article>
 
-              <article v-if="pageOfficeStaff">
-                <ContentRenderer
-                  :value="pageOfficeStaff.body"
-                  class="nuxt-content"
-                />
-              </article>
+                <div
+                  v-for="(member, index) in registrarMembers"
+                  :key="member!.slug"
+                  class="gallery"
+                  :class="{ 'left-column': index % 2 === 0, 'right-column': index % 2 !== 0 }"
+                >
+                  <MemberCard
+                    :headline-level="2"
+                    :name="member!.name"
+                    :slug="member!.slug"
+                    :image="member!.picture"
+                    :with-image="true"
+                    :infos="member!.infos"
+                    :job-title="member!.mostRecentRole?.role"
+                    :width="300"
+                    :is-alive="member!.isAlive"
+                    :lang="member!.lang"
+                    :female-title="member!.femaleTitle"
+                  />
+                </div>
 
-              <div class="section-content nuxt-content">
-                <h3 :id="toSlug(t('court.organization.emeritus-and-honorary-members'))">
-                  {{ t('court.organization.emeritus-and-honorary-members') }}
-                </h3>
-              </div>
+                <article v-if="pageOfficeStaff">
+                  <ContentRenderer
+                    :value="pageOfficeStaff.body"
+                    class="nuxt-content"
+                  />
+                </article>
 
-              <h4 class="heading-h3">
-                {{ t('general.message.presidents') }}
-              </h4>
+                <div class="section-content nuxt-content">
+                  <h3 :id="toSlug(t('court.organization.emeritus-and-honorary-members'))">
+                    {{ t('court.organization.emeritus-and-honorary-members') }}
+                  </h3>
+                </div>
 
-              <div class="gallery d-flex justify-space-between flex-wrap">
-                <MemberCard
-                  v-for="member of aliveNonActivePresidentMembersHistoric"
-                  :key="member.slug"
-                  :headline-level="5"
-                  :slug="member.slug"
-                  :infos="member.infos"
-                  :is-small="true"
-                  :name="member.name"
-                  :with-image="false"
-                  :job-title="member.mostRecentRole?.role"
-                  :lang="member.lang"
-                  :is-alive="member.isAlive"
-                  :female-title="member.femaleTitle"
-                />
-              </div>
+                <h4 class="heading-h3">
+                  {{ t('general.message.presidents') }}
+                </h4>
 
-              <h4 class="heading-h3">
-                {{ t('general.message.judges') }}
-              </h4>
+                <div class="gallery d-flex justify-space-between flex-wrap">
+                  <MemberCard
+                    v-for="member of aliveNonActivePresidentMembersHistoric"
+                    :key="member.slug"
+                    :headline-level="5"
+                    :slug="member.slug"
+                    :infos="member.infos"
+                    :is-small="true"
+                    :name="member.name"
+                    :with-image="false"
+                    :job-title="member.mostRecentRole?.role"
+                    :lang="member.lang"
+                    :is-alive="member.isAlive"
+                    :female-title="member.femaleTitle"
+                  />
+                </div>
 
-              <div class="gallery d-flex justify-space-between flex-wrap">
-                <MemberCard
-                  v-for="member of aliveNonActiveJudgeMembersHistoric"
-                  :key="member.slug"
-                  :headline-level="5"
-                  :slug="member.slug"
-                  :infos="member.infos"
-                  :is-small="true"
-                  :name="member.name"
-                  :with-image="false"
-                  :job-title="member.mostRecentRole?.role"
-                  :lang="member.lang"
-                  :is-alive="member.isAlive"
-                  :female-title="member.femaleTitle"
-                />
-              </div>
+                <h4 class="heading-h3">
+                  {{ t('general.message.judges') }}
+                </h4>
 
-              <h4 class="heading-h3">
-                {{ t('general.message.legal-secretaries') }}
-              </h4>
+                <div class="gallery d-flex justify-space-between flex-wrap">
+                  <MemberCard
+                    v-for="member of aliveNonActiveJudgeMembersHistoric"
+                    :key="member.slug"
+                    :headline-level="5"
+                    :slug="member.slug"
+                    :infos="member.infos"
+                    :is-small="true"
+                    :name="member.name"
+                    :with-image="false"
+                    :job-title="member.mostRecentRole?.role"
+                    :lang="member.lang"
+                    :is-alive="member.isAlive"
+                    :female-title="member.femaleTitle"
+                  />
+                </div>
 
-              <div class="gallery d-flex justify-space-between flex-wrap">
-                <MemberCard
-                  v-for="member of aliveNonActiveOfficeStaffMembersHistoric"
-                  :key="member.slug"
-                  :headline-level="5"
-                  :slug="member.slug"
-                  :infos="member.infos"
-                  :is-small="true"
-                  :name="member.name"
-                  :with-image="false"
-                  :job-title="member.mostRecentRole?.role"
-                  :lang="member.lang"
-                  :is-alive="member.isAlive"
-                  :female-title="member.femaleTitle"
-                />
-              </div>
+                <h4 class="heading-h3">
+                  {{ t('general.message.legal-secretaries') }}
+                </h4>
 
-              <h4 class="heading-h3">
-                {{ t('general.message.registrars') }}
-              </h4>
+                <div class="gallery d-flex justify-space-between flex-wrap">
+                  <MemberCard
+                    v-for="member of aliveNonActiveOfficeStaffMembersHistoric"
+                    :key="member.slug"
+                    :headline-level="5"
+                    :slug="member.slug"
+                    :infos="member.infos"
+                    :is-small="true"
+                    :name="member.name"
+                    :with-image="false"
+                    :job-title="member.mostRecentRole?.role"
+                    :lang="member.lang"
+                    :is-alive="member.isAlive"
+                    :female-title="member.femaleTitle"
+                  />
+                </div>
 
-              <div class="gallery d-flex justify-space-between flex-wrap">
-                <MemberCard
-                  v-for="member of aliveNonActiveRegistrarMembersHistoric"
-                  :key="member.slug"
-                  :headline-level="5"
-                  :slug="member.slug"
-                  :infos="member.infos"
-                  :is-small="true"
-                  :name="member.name"
-                  :with-image="false"
-                  :job-title="member.roles[0]?.role"
-                  :lang="member.lang"
-                  :start-date="member.roles[0]?.startDate"
-                  :end-date="member.roles[0]?.endDate"
-                  :is-alive="member.isAlive"
-                  :female-title="member.femaleTitle"
-                />
-              </div>
+                <h4 class="heading-h3">
+                  {{ t('general.message.registrars') }}
+                </h4>
 
-              <div class="section-content nuxt-content">
-                <h3 :id="toSlug(t('court.organization.previous-incumbents'))">
-                  {{ t('court.organization.previous-incumbents') }}
-                </h3>
-              </div>
+                <div class="gallery d-flex justify-space-between flex-wrap">
+                  <MemberCard
+                    v-for="member of aliveNonActiveRegistrarMembersHistoric"
+                    :key="member.slug"
+                    :headline-level="5"
+                    :slug="member.slug"
+                    :infos="member.infos"
+                    :is-small="true"
+                    :name="member.name"
+                    :with-image="false"
+                    :job-title="member.roles[0]?.role"
+                    :lang="member.lang"
+                    :start-date="member.roles[0]?.startDate"
+                    :end-date="member.roles[0]?.endDate"
+                    :is-alive="member.isAlive"
+                    :female-title="member.femaleTitle"
+                  />
+                </div>
 
-              <h4 class="heading-h3">
-                {{ t('general.message.presidents') }}
-              </h4>
+                <div class="section-content nuxt-content">
+                  <h3 :id="toSlug(t('court.organization.previous-incumbents'))">
+                    {{ t('court.organization.previous-incumbents') }}
+                  </h3>
+                </div>
 
-              <div class="gallery d-flex justify-space-between flex-wrap">
-                <MemberCard
-                  v-for="(member, i) of presidentMembersHistoric"
-                  :key="`${member.slug}-${i}`"
-                  :headline-level="5"
-                  :slug="member.slug"
-                  :infos="member.infos"
-                  :is-small="true"
-                  :name="member.name"
-                  :with-image="false"
-                  :job-title="member.roles[0]?.role"
-                  :lang="member.lang"
-                  :start-date="member.roles[0]?.startDate"
-                  :end-date="member.roles[0]?.endDate"
-                  :is-alive="member.isAlive"
-                  :female-title="member.femaleTitle"
-                />
-              </div>
+                <h4 class="heading-h3">
+                  {{ t('general.message.presidents') }}
+                </h4>
 
-              <h4 class="heading-h3">
-                {{ t('general.message.judges') }}
-              </h4>
+                <div class="gallery d-flex justify-space-between flex-wrap">
+                  <MemberCard
+                    v-for="(member, i) of presidentMembersHistoric"
+                    :key="`${member.slug}-${i}`"
+                    :headline-level="5"
+                    :slug="member.slug"
+                    :infos="member.infos"
+                    :is-small="true"
+                    :name="member.name"
+                    :with-image="false"
+                    :job-title="member.roles[0]?.role"
+                    :lang="member.lang"
+                    :start-date="member.roles[0]?.startDate"
+                    :end-date="member.roles[0]?.endDate"
+                    :is-alive="member.isAlive"
+                    :female-title="member.femaleTitle"
+                  />
+                </div>
 
-              <div class="gallery d-flex justify-space-between flex-wrap">
-                <MemberCard
-                  v-for="(member, i) of judgeMembersHistoric"
-                  :key="`${member.slug}-${i}`"
-                  :headline-level="5"
-                  :slug="member.slug"
-                  :infos="member.infos"
-                  :is-small="true"
-                  :name="member.name"
-                  :with-image="false"
-                  :job-title="member.roles[0]?.role"
-                  :lang="member.lang"
-                  :start-date="member.roles[0]?.startDate"
-                  :end-date="member.roles[0]?.endDate"
-                  :is-alive="member.isAlive"
-                  :female-title="member.femaleTitle"
-                />
-              </div>
-            </ClientOnly>
+                <h4 class="heading-h3">
+                  {{ t('general.message.judges') }}
+                </h4>
+
+                <div class="gallery d-flex justify-space-between flex-wrap">
+                  <MemberCard
+                    v-for="(member, i) of judgeMembersHistoric"
+                    :key="`${member.slug}-${i}`"
+                    :headline-level="5"
+                    :slug="member.slug"
+                    :infos="member.infos"
+                    :is-small="true"
+                    :name="member.name"
+                    :with-image="false"
+                    :job-title="member.roles[0]?.role"
+                    :lang="member.lang"
+                    :start-date="member.roles[0]?.startDate"
+                    :end-date="member.roles[0]?.endDate"
+                    :is-alive="member.isAlive"
+                    :female-title="member.femaleTitle"
+                  />
+                </div>
+              </ClientOnly>
+            </template>
           </v-row>
         </v-col>
       </v-row>
--- content/en/presentation-organization-judge.md
+++ content/en/presentation-organization-judge.md
@@ -10,7 +10,7 @@ locale: fr
 La Cour est composée de douze juges, nommés à vie par le Roi sur une liste double présentée alternativement par la Chambre des représentants et le Sénat. Cette liste est adoptée à la majorité des deux tiers au moins des suffrages des membres présents.
 <br>
 <br>
-<img src="/img/Y-0050.jpg" alt="Photo of the judges and registrars of the Constitutional Court" width="3000" height="2000" style="width:80%;height:auto;" />
+<img src="/img/Y-0050.jpg" alt="Photo of the judges and registrars of the Constitutional Court" width="3000" height="2000" style="display:block;margin-left:auto;margin-right:auto;width:80%;height:auto;text-align:center;" />
 <br>
 Six juges appartiennent au groupe linguistique français, six au groupe linguistique néerlandais. Un des juges doit avoir une connaissance suffisante de l’allemand. Dans chaque groupe linguistique, trois juges sont nommés sur la base de leur expérience juridique (professeur de droit dans une université belge, magistrat à la Cour de cassation ou au Conseil d’État ou référendaire à la Cour constitutionnelle) et trois juges sont nommés en raison d’une expérience de cinq ans au moins comme membre d’une assemblée parlementaire. La Cour est composée de juges de sexe différent, à raison au moins d’un tiers pour le groupe le moins nombreux, étant entendu que ce groupe doit être représenté dans les deux catégories professionnelles précitées.
 
