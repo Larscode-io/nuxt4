50d10d96390e7f7f6d685d082ba6881f698e937f
Larscode-io <lars.devocht@const-court.be>
2025-07-04 22:03:53 +0200

    feat: add design collection and visual audit content

---
+++ app/pages/demo2/design.vue
@@ -0,0 +1,22 @@
+<!-- Content based Page -->
+
+<script setup lang="ts">
+
+const { data: page } = await useAsyncData(
+  '/design/visual-audit',
+  () => queryCollection('collection_design').first()
+)
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div v-if="page">
+          <ContentRenderer :value="page"/>
+        </div>
+        <p v-else>No content available.</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
