adeff94e615e08b81646aaa632fe2e3d208b9367
Larscode-io <lars.devocht@const-court.be>
2025-07-28 10:44:57 +0200

    feat: implement sitemap generation for SEO with locale support

--- server/routes/robots.txt.ts
+++ server/routes/robots.txt.ts
@@ -1,21 +1,19 @@
 // server/routes/robots.txt.ts
-// this will expose the robots.txt file at /robots.txt
-// todo: make sure this file is served correctly in production
-
 export default defineEventHandler((event) => {
   const host = getRequestURL(event).host
   const isStaging = host.includes('nuxt.const-court.be')
 
-  // Disallow: /search/
-  // Sitemap: https://www.const-court.be/sitemap.xml
+  const sitemap = `Sitemap: https://${host}/sitemap.xml`
+
   const production = `
 User-agent: *
 Disallow: /public/
+${sitemap}
   `.trim()
 
-  // Disallow: /
   const staging = `
 User-agent: *
+Disallow: /
   `.trim()
 
   setHeader(event, 'Content-Type', 'text/plain')
---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,53 @@
+export default defineEventHandler((event) => {
+  const host = getRequestHeader(event, 'host') || ''
+  const supportedLocales = ['nl', 'fr', 'en', 'de']
+
+  // Herken lokale omgeving
+  const isLocal = host.includes('localhost') || host.includes('127.0.0.1')
+
+  const domainPrefix = isLocal
+    ? 'nl' // kies een default voor lokale test
+    : host.split('.')[0]
+
+  const locale = supportedLocales.includes(domainPrefix) ? domainPrefix : 'nl'
+  const baseUrl = isLocal
+    ? `http://${host}` // laat baseUrl in dev gewoon localhost:3003 zijn
+    : `https://${domainPrefix}.const-court.be`
+
+
+  const pathList = [
+    '/judgments',
+    '/court/presentation/jurisdiction',
+    '/court/presentation/organization',
+    '/search/judgment',
+    '/court/basic-text',
+    '/court/presentation/history-of-the-court',
+    '/judgments/pending-cases',
+    '/court/presentation/how-the-court-operates',
+    '/court/presentation/publications-on-the-court',
+    '/media/press-releases-concerning-the-judgments',
+  ]
+
+  const today = new Date().toISOString().split('T')[0]
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${pathList.map(path => {
+    const alternates = supportedLocales.map(lang => {
+      return `<xhtml:link rel="alternate" hreflang="${lang}" href="https://${lang}.const-court.be${path}" />`
+    }).join('\n    ')
+
+    return `
+  <url>
+    <loc>${baseUrl}${path}</loc>
+    ${alternates}
+    <lastmod>${today}</lastmod>
+  </url>`
+  }).join('')}
+</urlset>`
+
+  setResponseHeader(event, 'Content-Type', 'application/xml')
+  return xml
+})
