4c9f20b09c6da4458ac6cad5d103c29450757cfe
Larscode-io <lars.devocht@const-court.be>
2025-07-28 13:26:51 +0200

    refactor: improve comments for clarity in robots.txt handling

--- server/routes/robots.txt.ts
+++ server/routes/robots.txt.ts
@@ -1,15 +1,20 @@
 // server/routes/robots.txt.ts
+// todo:
+// wanneer we www.const-court.be effectief onder Nuxt 4 laat draaien,
+// zodat we caching, canonical-URL’s en fallbackregels correct mee afstemmen.
+
 export default defineEventHandler((event) => {
   const url = getRequestURL(event)
   const host = url.host
 
   const sitemap = `Sitemap: https://${host}/sitemap.xml`
 
-  const subdomain = host.split('.')[0] // nl, fr, de, en, www, ...
+  const subdomain = host.split('.')[0] // nl, fr, de, en, www, etc.
 
+  // Standaard: verbied toegang tot /public/
   let disallowRules = 'Disallow: /public/'
 
-  // Enkel toegang geven tot /public/{taal}/ als de subdomein een geldige taalcode is
+  // Enkel toegang geven tot /public/{taalcode}/ als subdomein exact nl, fr, de of en is
   const allowedLangs = ['nl', 'fr', 'de', 'en']
   if (allowedLangs.includes(subdomain)) {
     disallowRules = `
@@ -27,4 +32,5 @@ ${sitemap}
   setHeader(event, 'Content-Type', 'text/plain')
   return content
 })
+
 
